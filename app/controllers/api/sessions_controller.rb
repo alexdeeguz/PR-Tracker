b62810657f6dc8c6812e0eb5ed15a325c87adab4
alexdeeguz <alexdg316@gmail.com>
2020-04-01 18:00:16 -0700

    Add logger

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -12,7 +12,7 @@ class Api::SessionsController < ApplicationController
             login(@user)
             render 'api/users/show'
         else 
-            render json: ["invalid username or password"], status: 401
+            render json: ["invalid username or password"], status: 422
         end
     end
 
